48a481a597b4b1aa31ae534a5d46ad128006f312
nununuam <nunusnuam@gmail.com>
2021-12-10 20:45:59 -0800

    Randomize button and search function are complete

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 //When pressing search button, grab the value of input and assigned it to a variable
 $('#searchButton').on('click', function(){
   const keyWords = $('input').val();
-  console.log (keyWords);
+  
+  //reset the footer to empty
+  $('footer').empty();
 
 //Grab the string and input it into the API search url
   const promise = $.ajax({
-    url: `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${encodeURIComponent(keyWords)}`,
+    url: `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${encodeURIComponent(keyWords)}&hasImages=true`,
   });
   
   //If it works, then grab the data form that search word
   promise.then(
     (data) => {
-      console.log(data);
+
       //asign the array of key number into a variable
       const keyNum = data.objectIDs;
+
+      if(!keyNum){
+        $('.image').html(`<p class ="badRequest">Try another word.</p>`)
+        return;
+      }
       //asign the length of the array into a variable +1, so when randomize it can be floor and get the last index
       const arrayList = keyNum.length +1;
       //randomize the arrayList
@@ -22,9 +29,6 @@ $('#searchButton').on('click', function(){
       //Let user know how may images that contain the word they search for
       $('.infos').html(`<p id = "numOfImg">There are ${arrayList} images that contain the word you searched for, "${keyWords}".`);
 
-      console.log(arrayList);
-      console.log(randomKey);
-
       //grab the specific key number from random indecis and put it into the object api
       const promise1 = $.ajax({
         url: `https://collectionapi.metmuseum.org/public/collection/v1/objects/${keyNum[randomKey]}`,
@@ -32,10 +36,9 @@ $('#searchButton').on('click', function(){
       
       promise1.then(
         (keyData) => {
-          console.log(keyData);
+
           //Grab link of the image and display it in the html
           const img = keyData.primaryImage;
-          console.log(img)
           $('.image').html(`<img src = ${img}>`)
 
           //Assigned different type of data to a variable
@@ -59,8 +62,16 @@ $('#searchButton').on('click', function(){
   
 });
 
+
 $('#randomButton').on('click', function(){
-  console.log('clicking');
+
+  //add loader image
+  $('.image').html('<div class="lds-dual-ring"></div>')
+  //empty input value
+  $('input').val('');
+  //add disclaimer
+  $('footer').html('There are some with no image and differernt informations are unknown.');
+  $('.infos').empty();
 
   //grab all the objects from this api
   const promise2 = $.ajax({
@@ -70,12 +81,11 @@ $('#randomButton').on('click', function(){
   //if grabbing an api is successful continue
   promise2.then(
     (keys) => {
-
+    
       //grabs the array 
       const objectsIdsArray = keys.objectIDs;
       //randomize a number between o and a number less then the length of the array 
-      const randomizeIDs = Math.floor(Math.random() * objectsIdsArray.length+1)
-      console.log(objectsIdsArray[randomizeIDs]);
+      const randomizeIDs = Math.floor(Math.random() * objectsIdsArray.length)
       
       //Grab a id from particular index determined by random from this api
       const promise4 = $.ajax({
@@ -87,7 +97,6 @@ $('#randomButton').on('click', function(){
 
         //grabbing the image url from the data and displaying on html
         const img = aID.primaryImage;
-          console.log(img)
           $('.image').html(`<img src = ${img}>`)
 
           //Assigned different type of data to a variable
@@ -95,7 +104,7 @@ $('#randomButton').on('click', function(){
           const artistName= aID.artistDisplayName;
           const bio = aID.artistDisplayBio;
           const medium = aID.medium;
-          
+
           //displaying different type of data in the html
           $('.keyInfos').html(`<p>Title: ${artistName} <br/>Bio: ${bio} <br/>Department: ${department} </br>Medium: ${medium}</p>`);
       });
